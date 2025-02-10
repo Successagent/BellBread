9c27a2f844184cb251ceb91b9ffab018f61d6d96
Success Agent <miesineagent@gmail.com>
2025-02-10 18:52:14 +0100

    facebook-visiting-site

--- src/Movies/index.jsx
+++ src/Movies/index.jsx
@@ -21,7 +21,7 @@ const Movies = () => {
           <ul>
             {download_links?.map((links, index) => (
               <li key={index}>
-                <a target="_blank" href={links} rel="noopener noreferrer">
+                <a  href={links} rel="noopener noreferrer">
                   Download link {index + 1}
                 </a>
               </li>
